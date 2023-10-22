a30136d41fe5bfcf5fe74ad6e94b72d6b8117d9f
Mehmet <394822@ogr.ktu.edu.tr>
2023-10-22 14:26:50 +0300

    EMAIL and API token added

---
+++ prisma/migrations/20231022105241_auth_tokenss/migration.sql
@@ -0,0 +1,35 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "username" TEXT,
+    "image" TEXT,
+    "bio" TEXT,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_User" ("bio", "createdAt", "email", "id", "image", "isVerified", "name", "updatedAt", "username") SELECT "bio", "createdAt", "email", "id", "image", "isVerified", "name", "updatedAt", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE TABLE "new_Token" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "type" TEXT NOT NULL,
+    "emailToken" TEXT,
+    "valid" BOOLEAN NOT NULL DEFAULT true,
+    "expiration" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Token" ("createdAt", "emailToken", "expiration", "id", "type", "updatedAt", "userId", "valid") SELECT "createdAt", "emailToken", "expiration", "id", "type", "updatedAt", "userId", "valid" FROM "Token";
+DROP TABLE "Token";
+ALTER TABLE "new_Token" RENAME TO "Token";
+CREATE UNIQUE INDEX "Token_emailToken_key" ON "Token"("emailToken");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,100 +1,77 @@
-import { Router }from 'express';
+import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
 
 const router = Router();
 const prisma = new PrismaClient();
 
+// Tweet CRUD
 
-//Create a tweet
+// Create Tweet
 router.post('/', async (req, res) => {
-    const { content, image, userId } = req.body;
-    try{
-        const result = await prisma.tweet.create({
-            data: {
-               content,
-                image,
-                userId,
-            },
-        });
-        res.json(result);
-    } catch (e) {
-        res.status(400).json({error: "Something went wrong"});
-    }   
+  const { content, image } = req.body;
+  // @ts-ignore
+  const user = req.user;
+
+  try {
+    const result = await prisma.tweet.create({
+      data: {
+        content,
+        image,
+        userId: user.id,
+      },
+      include: { user: true },
+    });
+
+    res.json(result);
+  } catch (e) {
+    res.status(400).json({ error: 'Username and email should be unique' });
+  }
 });
 
-// List tweets
+// list Tweet
 router.get('/', async (req, res) => {
-    const allTweets = await prisma.tweet.findMany({ 
-        //include: { user: true }
+  const allTweets = await prisma.tweet.findMany({
+    include: {
+      user: {
         select: {
-            id: true,
-            content: true,
-            user: {
-                select: {
-                    id: true,
-                    username: true,
-                    image: true,
-                },
-            },
+          id: true,
+          name: true,
+          username: true,
+          image: true,
         },
-    })
-    res.json(allTweets);
+      },
+    },
+  });
+  res.json(allTweets);
 });
 
-// Get a single tweet
+// get one Tweet
 router.get('/:id', async (req, res) => {
-    const {id} = req.params;
-    const tweet = await prisma.tweet.findUnique({
-        where: {
-            id: Number(id),
-        },
-        include: {
-            user: {
-                select: {
-                    id: true,
-                    username: true,
-                    image: true,
-                },
-            },
-        },
-    });
-    if (!tweet) {
-        return res.status(404).json({error: "Tweet not found"});
-    }
-    res.json(tweet);
+  const { id } = req.params;
+  console.log('Query tweet with id: ', id);
+
+  const tweet = await prisma.tweet.findUnique({
+    where: { id: Number(id) },
+    include: { user: true },
+  });
+  if (!tweet) {
+    return res.status(404).json({ error: 'Tweet not found!' });
+  }
+
+  res.json(tweet);
 });
 
-// Update a tweet
-router.put('/:id', async (req, res) => {
-    const {id} = req.params;
-    const {content, image} = req.body;
-
-    try{
-        const updatedTweet = await prisma.tweet.update({
-            where: {
-                id: Number(id),
-            },
-            data: {
-                content,
-                image,
-            },
-        });
-        res.json(updatedTweet);
-    } catch (e) {
-        res.status(400).json({error: "Failed to update the tweet"});
-    }
+// update Tweet
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(501).json({ error: `Not Implemented: ${id}` });
 });
 
-// Delete a tweet
+// delete Tweet
 router.delete('/:id', async (req, res) => {
-    const {id} = req.params;
-    await prisma.tweet.delete({
-        where: {
-            id: Number(id),
-        },
-    });
-    res.sendStatus(204);
+  const { id } = req.params;
+  await prisma.tweet.delete({ where: { id: Number(id) } });
+  res.sendStatus(200);
 });
-
 
 export default router;
